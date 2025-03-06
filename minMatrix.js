a2d1ce6589c33a0bd5b5758ccdf8238637591f96
Kaito <113097767+KaitoIto11235@users.noreply.github.com>
2025-03-06 21:25:11 +0900

    Update minMatrix.js

--- minMatrix.js
+++ minMatrix.js
@@ -1,6 +1,6 @@
 // ------------------------------------------------------------------------------------------------
-// minMatrix.js
-// version 0.0.1
+// minMatrixb.js
+// version 0.0.2
 // Copyright (c) doxas
 // ------------------------------------------------------------------------------------------------
 
@@ -97,16 +97,16 @@ function matIV(){
 		} else {
 			dest = mat;
 		}
-		dest[0] = g * s + k * t + o * u;
-		dest[1] = h * s + l * t + p * u;
-		dest[2] = i * s + m * t + q * u;
-		dest[3] = j * s + n * t + r * u;
-		dest[4] = g * v + k * w + o * x;
-		dest[5] = h * v + l * w + p * x;
-		dest[6] = i * v + m * w + q * x;
-		dest[7] = j * v + n * w + r * x;
-		dest[8] = g * y + k * z + o * A;
-		dest[9] = h * y + l * z + p * A;
+		dest[0]  = g * s + k * t + o * u;
+		dest[1]  = h * s + l * t + p * u;
+		dest[2]  = i * s + m * t + q * u;
+		dest[3]  = j * s + n * t + r * u;
+		dest[4]  = g * v + k * w + o * x;
+		dest[5]  = h * v + l * w + p * x;
+		dest[6]  = i * v + m * w + q * x;
+		dest[7]  = j * v + n * w + r * x;
+		dest[8]  = g * y + k * z + o * A;
+		dest[9]  = h * y + l * z + p * A;
 		dest[10] = i * y + m * z + q * A;
 		dest[11] = j * y + n * z + r * A;
 		return dest;
@@ -151,22 +151,44 @@ function matIV(){
 		var t = near * Math.tan(fovy * Math.PI / 360);
 		var r = t * aspect;
 		var a = r * 2, b = t * 2, c = far - near;
-		dest[0] = near * 2 / a;
-		dest[1] = 0;
-		dest[2] = 0;
-		dest[3] = 0;
-		dest[4] = 0;
-		dest[5] = near * 2 / b;
-		dest[6] = 0;
-		dest[7] = 0;
-		dest[8] = 0;
-		dest[9] = 0;
+		dest[0]  = near * 2 / a;
+		dest[1]  = 0;
+		dest[2]  = 0;
+		dest[3]  = 0;
+		dest[4]  = 0;
+		dest[5]  = near * 2 / b;
+		dest[6]  = 0;
+		dest[7]  = 0;
+		dest[8]  = 0;
+		dest[9]  = 0;
 		dest[10] = -(far + near) / c;
 		dest[11] = -1;
 		dest[12] = 0;
 		dest[13] = 0;
 		dest[14] = -(far * near * 2) / c;
 		dest[15] = 0;
+		return dest;
+	};
+	this.ortho = function(left, right, top, bottom, near, far, dest) {
+		var h = (right - left);
+		var v = (top - bottom);
+		var d = (far - near);
+		dest[0]  = 2 / h;
+		dest[1]  = 0;
+		dest[2]  = 0;
+		dest[3]  = 0;
+		dest[4]  = 0;
+		dest[5]  = 2 / v;
+		dest[6]  = 0;
+		dest[7]  = 0;
+		dest[8]  = 0;
+		dest[9]  = 0;
+		dest[10] = -2 / d;
+		dest[11] = 0;
+		dest[12] = -(left + right) / h;
+		dest[13] = -(top + bottom) / v;
+		dest[14] = -(far + near) / d;
+		dest[15] = 1;
 		return dest;
 	};
 	this.transpose = function(mat, dest){
@@ -210,4 +232,269 @@ function matIV(){
 		dest[15] = ( i * t - j * r + k * q) * ivd;
 		return dest;
 	};
+}
+
+function qtnIV(){
+	this.create = function(){
+		return new Float32Array(4);
+	};
+	this.identity = function(dest){
+		dest[0] = 0; dest[1] = 0; dest[2] = 0; dest[3] = 1;
+		return dest;
+	};
+	this.inverse = function(qtn, dest){
+		dest[0] = -qtn[0];
+		dest[1] = -qtn[1];
+		dest[2] = -qtn[2];
+		dest[3] =  qtn[3];
+		return dest;
+	};
+	this.normalize = function(dest){
+		var x = dest[0], y = dest[1], z = dest[2], w = dest[3];
+		var l = Math.sqrt(x * x + y * y + z * z + w * w);
+		if(l === 0){
+			dest[0] = 0;
+			dest[1] = 0;
+			dest[2] = 0;
+			dest[3] = 0;
+		}else{
+			l = 1 / l;
+			dest[0] = x * l;
+			dest[1] = y * l;
+			dest[2] = z * l;
+			dest[3] = w * l;
+		}
+		return dest;
+	};
+	this.multiply = function(qtn1, qtn2, dest){
+		var ax = qtn1[0], ay = qtn1[1], az = qtn1[2], aw = qtn1[3];
+		var bx = qtn2[0], by = qtn2[1], bz = qtn2[2], bw = qtn2[3];
+		dest[0] = ax * bw + aw * bx + ay * bz - az * by;
+		dest[1] = ay * bw + aw * by + az * bx - ax * bz;
+		dest[2] = az * bw + aw * bz + ax * by - ay * bx;
+		dest[3] = aw * bw - ax * bx - ay * by - az * bz;
+		return dest;
+	};
+	this.rotate = function(angle, axis, dest){
+		var sq = Math.sqrt(axis[0] * axis[0] + axis[1] * axis[1] + axis[2] * axis[2]);
+		if(!sq){return null;}
+		var a = axis[0], b = axis[1], c = axis[2];
+		if(sq != 1){sq = 1 / sq; a *= sq; b *= sq; c *= sq;}
+		var s = Math.sin(angle * 0.5);
+		dest[0] = a * s;
+		dest[1] = b * s;
+		dest[2] = c * s;
+		dest[3] = Math.cos(angle * 0.5);
+		return dest;
+	};
+	this.toVecIII = function(vec, qtn, dest){
+		var qp = this.create();
+		var qq = this.create();
+		var qr = this.create();
+		this.inverse(qtn, qr);
+		qp[0] = vec[0];
+		qp[1] = vec[1];
+		qp[2] = vec[2];
+		this.multiply(qp, qr, qq);
+		//this.multiply(qr, qp, qq);
+		this.multiply(qtn, qq, qr);		// おそらくこっちが正しい qvq^(-1)
+		//this.multiply(qq, qtn, qr);
+		dest[0] = qr[0];
+		dest[1] = qr[1];
+		dest[2] = qr[2];
+		return dest;
+	};
+	this.toMatIV = function(qtn, dest){
+		var x = qtn[0], y = qtn[1], z = qtn[2], w = qtn[3];
+		var x2 = x + x, y2 = y + y, z2 = z + z;
+		var xx = x * x2, xy = x * y2, xz = x * z2;
+		var yy = y * y2, yz = y * z2, zz = z * z2;
+		var wx = w * x2, wy = w * y2, wz = w * z2;
+		dest[0]  = 1 - (yy + zz);
+		dest[1]  = xy - wz;
+		dest[2]  = xz + wy;
+		dest[3]  = 0;
+		dest[4]  = xy + wz;
+		dest[5]  = 1 - (xx + zz);
+		dest[6]  = yz - wx;
+		dest[7]  = 0;
+		dest[8]  = xz - wy;
+		dest[9]  = yz + wx;
+		dest[10] = 1 - (xx + yy);
+		dest[11] = 0;
+		dest[12] = 0;
+		dest[13] = 0;
+		dest[14] = 0;
+		dest[15] = 1;
+		return dest;
+	};
+	this.slerp = function(qtn1, qtn2, time, dest){
+		var ht = qtn1[0] * qtn2[0] + qtn1[1] * qtn2[1] + qtn1[2] * qtn2[2] + qtn1[3] * qtn2[3];
+		var hs = 1.0 - ht * ht;
+		if(hs <= 0.0){
+			dest[0] = qtn1[0];
+			dest[1] = qtn1[1];
+			dest[2] = qtn1[2];
+			dest[3] = qtn1[3];
+		}else{
+			hs = Math.sqrt(hs);
+			if(Math.abs(hs) < 0.0001){
+				dest[0] = (qtn1[0] * 0.5 + qtn2[0] * 0.5);
+				dest[1] = (qtn1[1] * 0.5 + qtn2[1] * 0.5);
+				dest[2] = (qtn1[2] * 0.5 + qtn2[2] * 0.5);
+				dest[3] = (qtn1[3] * 0.5 + qtn2[3] * 0.5);
+			}else{
+				var ph = Math.acos(ht);
+				var pt = ph * time;
+				var t0 = Math.sin(ph - pt) / hs;
+				var t1 = Math.sin(pt) / hs;
+				dest[0] = qtn1[0] * t0 + qtn2[0] * t1;
+				dest[1] = qtn1[1] * t0 + qtn2[1] * t1;
+				dest[2] = qtn1[2] * t0 + qtn2[2] * t1;
+				dest[3] = qtn1[3] * t0 + qtn2[3] * t1;
+			}
+		}
+		return dest;
+	};
+}
+
+function torus(row, column, irad, orad, color){
+	var pos = new Array(), nor = new Array(),
+	    col = new Array(), st  = new Array(), idx = new Array();
+	for(var i = 0; i <= row; i++){
+		var r = Math.PI * 2 / row * i;
+		var rr = Math.cos(r);
+		var ry = Math.sin(r);
+		for(var ii = 0; ii <= column; ii++){
+			var tr = Math.PI * 2 / column * ii;
+			var tx = (rr * irad + orad) * Math.cos(tr);
+			var ty = ry * irad;
+			var tz = (rr * irad + orad) * Math.sin(tr);
+			var rx = rr * Math.cos(tr);
+			var rz = rr * Math.sin(tr);
+			if(color){
+				var tc = color;
+			}else{
+				tc = hsva(360 / column * ii, 1, 1, 1);
+			}
+			var rs = 1 / column * ii;
+			var rt = 1 / row * i + 0.5;
+			if(rt > 1.0){rt -= 1.0;}
+			rt = 1.0 - rt;
+			pos.push(tx, ty, tz);
+			nor.push(rx, ry, rz);
+			col.push(tc[0], tc[1], tc[2], tc[3]);
+			st.push(rs, rt);
+		}
+	}
+	for(i = 0; i < row; i++){
+		for(ii = 0; ii < column; ii++){
+			r = (column + 1) * i + ii;
+			idx.push(r, r + column + 1, r + 1);
+			idx.push(r + column + 1, r + column + 2, r + 1);
+		}
+	}
+	return {p : pos, n : nor, c : col, t : st, i : idx};
+}
+
+function sphere(row, column, rad, color){
+	var pos = new Array(), nor = new Array(),
+	    col = new Array(), st  = new Array(), idx = new Array();
+	for(var i = 0; i <= row; i++){
+		var r = Math.PI / row * i;
+		var ry = Math.cos(r);
+		var rr = Math.sin(r);
+		for(var ii = 0; ii <= column; ii++){
+			var tr = Math.PI * 2 / column * ii;
+			var tx = rr * rad * Math.cos(tr);
+			var ty = ry * rad;
+			var tz = rr * rad * Math.sin(tr);
+			var rx = rr * Math.cos(tr);
+			var rz = rr * Math.sin(tr);
+			if(color){
+				var tc = color;
+			}else{
+				tc = hsva(360 / row * i, 1, 1, 1);
+			}
+			pos.push(tx, ty, tz);
+			nor.push(rx, ry, rz);
+			col.push(tc[0], tc[1], tc[2], tc[3]);
+			st.push(1 - 1 / column * ii, 1 / row * i);
+		}
+	}
+	r = 0;
+	for(i = 0; i < row; i++){
+		for(ii = 0; ii < column; ii++){
+			r = (column + 1) * i + ii;
+			idx.push(r, r + 1, r + column + 2);
+			idx.push(r, r + column + 2, r + column + 1);
+		}
+	}
+	return {p : pos, n : nor, c : col, t : st, i : idx};
+}
+
+function cube(side, color){
+	var hs = side * 0.5;
+	var pos = [
+		-hs, -hs,  hs,  hs, -hs,  hs,  hs,  hs,  hs, -hs,  hs,  hs,
+		-hs, -hs, -hs, -hs,  hs, -hs,  hs,  hs, -hs,  hs, -hs, -hs,
+		-hs,  hs, -hs, -hs,  hs,  hs,  hs,  hs,  hs,  hs,  hs, -hs,
+		-hs, -hs, -hs,  hs, -hs, -hs,  hs, -hs,  hs, -hs, -hs,  hs,
+		 hs, -hs, -hs,  hs,  hs, -hs,  hs,  hs,  hs,  hs, -hs,  hs,
+		-hs, -hs, -hs, -hs, -hs,  hs, -hs,  hs,  hs, -hs,  hs, -hs
+	];
+	var nor = [
+		-1.0, -1.0,  1.0,  1.0, -1.0,  1.0,  1.0,  1.0,  1.0, -1.0,  1.0,  1.0,
+		-1.0, -1.0, -1.0, -1.0,  1.0, -1.0,  1.0,  1.0, -1.0,  1.0, -1.0, -1.0,
+		-1.0,  1.0, -1.0, -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0,
+		-1.0, -1.0, -1.0,  1.0, -1.0, -1.0,  1.0, -1.0,  1.0, -1.0, -1.0,  1.0,
+		 1.0, -1.0, -1.0,  1.0,  1.0, -1.0,  1.0,  1.0,  1.0,  1.0, -1.0,  1.0,
+		-1.0, -1.0, -1.0, -1.0, -1.0,  1.0, -1.0,  1.0,  1.0, -1.0,  1.0, -1.0
+	];
+	var col = new Array();
+	for(var i = 0; i < pos.length / 3; i++){
+		if(color){
+			var tc = color;
+		}else{
+			tc = hsva(360 / pos.length / 3 * i, 1, 1, 1);
+		}
+		col.push(tc[0], tc[1], tc[2], tc[3]);
+	}
+	var st = [
+		0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0
+	];
+	var idx = [
+		 0,  1,  2,  0,  2,  3,
+		 4,  5,  6,  4,  6,  7,
+		 8,  9, 10,  8, 10, 11,
+		12, 13, 14, 12, 14, 15,
+		16, 17, 18, 16, 18, 19,
+		20, 21, 22, 20, 22, 23
+	];
+	return {p : pos, n : nor, c : col, t : st, i : idx};
+}
+
+function hsva(h, s, v, a){
+	if(s > 1 || v > 1 || a > 1){return;}
+	var th = h % 360;
+	var i = Math.floor(th / 60);
+	var f = th / 60 - i;
+	var m = v * (1 - s);
+	var n = v * (1 - s * f);
+	var k = v * (1 - s * (1 - f));
+	var color = new Array();
+	if(!s > 0 && !s < 0){
+		color.push(v, v, v, a); 
+	} else {
+		var r = new Array(v, n, m, m, k, v);
+		var g = new Array(k, v, v, n, m, m);
+		var b = new Array(m, m, k, v, v, n);
+		color.push(r[i], g[i], b[i], a);
+	}
+	return color;
 }
